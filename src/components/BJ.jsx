777aee127ac57de95d966e147c667d701677ca0d
jupalibalaji <77019036+jupalibalaji@users.noreply.github.com>
2024-08-15 01:50:44 -0400

    added few changes

--- src/components/BJ.jsx
+++ src/components/BJ.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import profilePic from "../assets/KevinRushProfile.png";
+import profilePic from "../assets/1.jpeg";
 import { HERO_CONTENT } from "../constants";
 
 const BJ = () => {
@@ -13,9 +13,9 @@ const BJ = () => {
               animate={{ x: 0 }}
               initial={{ x: -200 }}
               transition={{ ease: "easeOut", duration: 1.5 }}
-              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl" // Slightly reduced font size
             >
-              Balaji Jupalli
+              BALAJI JUPALLI
             </motion.h1>
 
             {/* Fade-in with color animation for the title */}
@@ -23,7 +23,7 @@ const BJ = () => {
               animate={{ opacity: 1 }}
               initial={{ opacity: 0 }}
               transition={{ ease: "easeIn", duration: 2 }}
-              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent" // Slightly reduced font size
             >
               Full Stack Developer
             </motion.span>
@@ -33,7 +33,7 @@ const BJ = () => {
               animate={{ x: 0 }}
               initial={{ x: 200 }}
               transition={{ ease: "easeOut", duration: 1.5 }}
-              className="my-2 max-w-xl py-6 font-light tracking-tighter"
+              className="my-2 max-w-xl py-6 font-light text-base tracking-tighter" // Slightly reduced font size
             >
               {HERO_CONTENT}
             </motion.p>
